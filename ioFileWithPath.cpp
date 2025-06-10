9290e5934e77747172bfa9828a7f47f858592acf
fadilsyy <fadilasytar@gmail.com>
2025-06-10 18:25:07 +0700

    kondisi if

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -30,4 +30,10 @@ int main() {
     // menunjuk ke sebuah file
     infile.open(NamaFile, ios::in);
     cout << endl << "=> Membuka dan membaca file " << endl; 
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        /* code */
+    }
+    
 }
