853569c690167b449ea21376f5834b1d6bb7e97d
fadilsyy <fadilasytar@gmail.com>
2025-06-10 17:33:41 +0700

    menutup file

--- ioFile.cpp
+++ ioFile.cpp
@@ -21,5 +21,9 @@ int main(){
         // menulis dan memasukkan nilai dari "baris" ke dalam file
         outfile << baris << endl;
     }
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+    // membuka file dalam mode membaca
+    ifstream infile;
     
 }
