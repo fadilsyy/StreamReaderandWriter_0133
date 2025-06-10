8192fb9e8d42d73610532750ec6cca7c4d4445aa
fadilsyy <fadilasytar@gmail.com>
2025-06-10 17:39:32 +0700

    untuk membuka dan menutup file

--- ioFile.cpp
+++ ioFile.cpp
@@ -25,5 +25,20 @@ int main(){
     outfile.close();
     // membuka file dalam mode membaca
     ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sini
+            cout << baris << '\n' ;
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
     
 }
