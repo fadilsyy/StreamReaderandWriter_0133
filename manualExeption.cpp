de9ca520dcbe0f4eaefabd5c7159f8e007003458
fadilsyy <fadilasytar@gmail.com>
2025-06-10 18:44:22 +0700

    try dan throw

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0,5; // melemparkan sbuah integer maka
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
     
 }
